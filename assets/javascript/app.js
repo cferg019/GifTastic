5f1891ebd435202bef5a11580d9706db2c33ec3f
Casey Ferguson <cferg019@gmail.com>
2019-11-22 15:54:43 -0500

    still/animate gif function added, gifs formatted in line

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,33 +1,82 @@
-var animals = ["llama", "lions", "bears"];
+var tvShows = ["Spongebob Squarepants", "Parks & Rec", "Saved by the Bell", "Rocko's Modern Life", "Community", "The Office", "30 Rock"];
 
-function createButtons () {
+// function to loop through 'tvShows' array to append to button display div
+function createButtons() {
     $("#buttonDisplay").empty();
-
-    for (var i = 0; i < animals.length; i++) {
+    for (var i = 0; i < tvShows.length; i++) {
         var a = $("<button>");
-          // Adding a class
-          a.addClass("animal");
-          // Adding a data-attribute with a value of the movie at index i
-          a.attr("data-name", animals[i]);
-          // Providing the button's text with a value of the movie at index i
-          a.text(animals[i]);
-          // Adding the button to the HTML
-          $("#buttonDisplay").append(a);
+        a.addClass("tvButtons");
+        a.attr("data-show-name", tvShows[i]);
+        a.text(tvShows[i]);
+        a.on("click", handleButtonClick)
+        $("#buttonDisplay").append(a);
     }
 }
 
-$("#add-animal").on("click", function(event) {
-    // event.preventDefault() prevents the form from trying to submit itself.
-    // We're using a form so that the user can hit enter instead of clicking the button if they want
+// on click event for adding new tv show buttons and appending to button display div
+$("#addNewShow").on("click", function (event) {
     event.preventDefault();
-
-    // This line will grab the text from the input box
-    var animal = $("#newSearchInput").val().trim();
-    // The movie from the textbox is then added to our array
-    animals.push(animal);
-
-    // calling renderButtons which handles the processing of our movie array
+    var userShowInput = $("#newSearchInput").val().trim();
+    tvShows.push(userShowInput);
+    // clear input field after submitting option
+    $("#newSearchInput").val("")
     createButtons();
-  });
+});
+
+// function building on click event for buttons created in createButtons function
+function handleButtonClick(event) {
+    event.preventDefault();
+    var tvShowURLinput = $(this).attr("data-show-name");
+    //endcodeURI --> translates spaces in user input into readable string
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + encodeURI(tvShowURLinput) + "&api_key=EwRRkwAvsMmOUtLpLz92yNlczXJOCBlb&limit=25";
+    console.log(queryURL)
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        var results = response.data;
+
+        // clear out gif display before displaying next show's gifs 
+        $("#showGifsHere").empty();
+        // loop through object results to display the 12 gifs called
+        for (var i = 0; i < results.length; i++) {
+            var gifDiv = $("<div>");
+            var p = $("<p>");
+            var tvShowImage = $("<img>");
+
+            p = "Rating: " + results[i].rating + "<br>";
+            tvShowImage.attr("src", results[i].images.fixed_height_still.url)
+    
+            gifDiv.addClass("float-md-left");
+            gifDiv.append(p);
+            gifDiv.append(tvShowImage);
+
+            $("#showGifsHere").append(gifDiv);
+        }
+    })
+}
+
+$(document).on("click", "img", function() {
+    console.log("hi there");        
+
+});
+
+
+ $("body").on("click", "img", function() {
+    var src = $(this).attr("src");
+  if($(this).hasClass("playing")){
+     //stop
+     $(this).attr("src", src.replace(".gif", "_s.gif"))
+     $(this).removeClass("playing");
+  } else {
+    //play
+    $(this).addClass("playing");
+    $(this).attr("src", src.replace("_s.gif", ".gif"))
+  }
+});
+
+
 
 createButtons();
+
